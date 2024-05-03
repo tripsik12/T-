fac737cff741e9c9a90f8db6093fea9c598b8482
Opilane TTHK <opilane@tthk.ee>
2024-05-03 12:54:59 +0300

    3maiTRISTAN

---
+++ IfAndElse/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Töö
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+            
+            Console.WriteLine("Mis on sinu nimi?");
+
+            string name = Console.ReadLine();
+            if (name != null)
+            {
+                Console.WriteLine("Sisestasid enda nime:");
+                Console.WriteLine(name);
+            }
+            else
+            {
+                Console.WriteLine("ei sisestanud enda nime");
+            }
+        }
+    }
+}
